8acdd1fc3edbc57429e28f067db01dfa14e40762
Eugene Pankov <john.pankov@gmail.com>
2014-01-06 16:58:12 +0300

    .

--- 7 semester/MOP/3.py
+++ 7 semester/MOP/3.py
@@ -1,147 +1,123 @@
 import numpy as np
-import time
 
-A = np.matrix([
-    [1., 0., 2., 1.],
-    [0., 1., -1., 2.],
+
+A = np.matrix([10, 10, 10]).T
+B = np.matrix([15, 15]).T
+C = np.matrix([
+    [1, 3],
+    [999, 4],
+    [2, 999],
 ])
 
-B = np.matrix([2., 3.]).T
-
-C = np.matrix([-8., -6., -4., -6.]).T
-D = np.matrix([
-    [2.,1.,1.,0.],
-    [1.,1.,0.,0.],
-    [1.,0.,1.,0.],
-    [0.,0.,0.,0.],
+"""
+A = np.matrix([30, 40, 20]).T
+B = np.matrix([20, 30, 30, 10]).T
+C = np.matrix([
+    [2, 3, 2, 4],
+    [3, 2, 5, 1],
+    [4, 3, 2, 6],
 ])
 
-X = np.matrix([2.,3.,0.,0.]).T
-Jstar = [0,1]
-Jb = [0,1]
 
-
-def raw_simplex(A, B, C, D, X, Jb, Jstar):
-    print '======= RAW SIMPLEX'
-    print 'A', A
-    print 'B', B
-    print 'C', C
-    print 'D', D
-    print 'X', X
-    print 'Jb', Jb
-    print '==================='
-
-    M, N = A.shape
-    J = range(0, N)
-    E = np.identity(M)
+"""
+def transport(A, B, C):
+    M, N = C.shape
+    I1 = range(M)
+    I2 = range(N)
+    U = []
+    Ub = []
+    x = np.zeros(C.shape)
+    for j in I2:
+        for i in I1:
+            U += [(i, j)]
+            rx = sum(x[ii, j] for ii in I1)
+            tx = sum(x[i, jj] for jj in I2)
+            x[i, j] = min(A[i, 0] - tx, B[j, 0] - rx)
+            if x[i, j] != 0:
+                Ub.append((i, j))
 
     step = 0
     while True:
         step += 1
-        print '\n-------------------'
-        print 'Step %i' % step
-        print 'A:', A
-        print 'D:', D
-        print 'X:', X
-        print 'Jb:', Jb
-        print 'J*:', Jstar
+        print '-------------'
+        print 'Step', step
+        print 'Base', Ub
+        print 'X:\n', x
+        print 'Cost:', np.sum(np.multiply(C, x))
 
-        Ab = A[:, Jb]
-        AbI = Ab.I
-        cx = C + D * X
-        cxb = cx[Jb]
-        ux = -cxb.T * AbI
+        u = [0] * M
+        v = [0] * N
 
-        delta = [ux * A[:, j] + cx[j] for j in J]
-        print 'delta', delta
-        if all(
-            delta[i] >= 0
-            for i in J
-            if i not in Jb
-        ):
-            print 'Done!'
-            return X, Jb
+        u[0] = 0
+        v[0] = C[0, 0] - v[0]
+        i = 0
+        j = 0
 
-        j0 = 0
-        for j in J:
-            if delta[j] < 0 and j not in Jb:
-                j0 = j
-                break
+        def potentialize(i, j):
+            if i < M - 1 and x[i + 1, j] > 0:
+                u[i + 1] = C[i + 1, j] - v[j]
+                potentialize(i + 1, j)
+            if j < N - 1 and x[i, j + 1] > 0:
+                v[j + 1] = C[i, j + 1] - u[i]
+                potentialize(i, j + 1)
 
-        print 'j0:', j0
+        potentialize(0, 0)
 
-        while True:
-            Astar = A[:, Jstar]
-            Dstar = D[Jstar, :][:, Jstar]
+        print 'UV', u, v
+        delta = np.zeros(C.shape)
+        for i in I1:
+            for j in I2:
+                delta[i, j] = u[i] + v[j] - C[i, j]
+        
 
-            H = np.concatenate([np.concatenate([Dstar, Astar]), np.concatenate([Astar.T, np.zeros([M, M])])], 1)
-            hj0 = np.concatenate([D[Jstar, j0], A[:, j0]])
-            ly = -H.I * hj0
-            lstar = ly[:len(Jstar)]
-            y = ly[len(Jstar):]
+        print 'Delta:\n', delta
 
-            print lstar, y
+        if all(delta[i,j] <= 0 for i,j in U if (i, j) not in Ub):
+            print 'Done'
+            return x
 
-            beta = D[Jstar, j0].T * lstar + A[:, j0].T * y + D[j0, j0]
+        i0 = j0 = 0
+        for i, j in U:
+            if not (i, j) in Ub:
+                if delta[i,j] > delta[i0, j0]:
+                    i0, j0 = i, j
 
-            theta = [
-                (-X[j] / lstar[Jstar.index(j)] if lstar[Jstar.index(j)] < 0 else float('inf'))
-                    if j in Jstar
-                    else None
-                for j in J
-            ]
-            theta[j0] = abs(delta[j0] / beta) if beta != 0 else float('inf')
-            theta0 = min(theta[j] for j in J if j in Jstar or j == j0)
-            print 'beta', beta
-            print 'theta', theta
-            print 'theta0', theta0
-            jstar = theta.index(theta0)
-            print 'j*', jstar
-            if theta0 == float('inf'):
-                print 'Conditions not compatible!'
-                return None, None
-            
-            X_ = [
-                float(X[j] + theta0 * lstar[Jstar.index(j)])
-                if j in Jstar
-                else (
-                    float(X[j0] + theta0)
-                    if j == j0
-                    else
-                        0
-                )
-                for j in J
-            ]
-            X = np.matrix(X_).T
+        all_directions = [(1,0), (-1,0), (0,1), (0,-1)]
+        def findloop(i, j, path, directions):
+            path = path + [(i, j)]
+            for dx,dy in directions:
+                ii = i
+                jj = j
+                for d in range(0, max(M, N)):
+                    ii += dy
+                    jj += dx
+                    if ii < 0 or jj < 0 or ii == M or jj == N:
+                        break
 
-            if jstar == j0:
-                Jstar.append(j0)
-                break
-            elif jstar in Jstar and jstar not in Jb:
-                Jstar.remove(jstar)
-                delta[j0] += theta0 * beta
-                continue
-            elif jstar in Jb and any(
-                    E[:, jstar].T * A.I[:, Jb] * A[:, jplus] != 0
-                    for jplus in Jstar
-                    if jplus not in Jb
-                ):
-                for jplus in Jstar:
-                    if jplus not in Jb:
-                        if E[:, jstar].T * A.I[:, Jb] * A[:, jplus] != 0:
-                            print 'j+', jplus
-                            break
-                Jb.remove(jstar)
-                Jb.append(jplus)
-                Jstar.remove(jstar)
-                delta[j0] += theta0 * beta
-                continue
-            else:
-                Jb.remove(jstar)
-                Jb.append(j0)
-                Jstar.remove(jstar)
-                Jstar.append(j0)
-                break
-        time.sleep(0.5)
+                    if (ii, jj) == path[0]:
+                        return path
 
-raw_simplex(A, B, C, D, X, Jb, Jstar)
+                    if x[ii, jj] > 0:
+                        dirs = all_directions[:]
+                        dirs.remove((dx, dy))
+                        dirs.remove((-dx, -dy))
+                        r = findloop(ii, jj, path, dirs)
+                        if r:
+                            return r
+                        break
+
+        loop = findloop(i0, j0, [], [(1,0), (-1,0)])
+        print 'Loop from', i0, j0
+        disp = '\n'.join(''.join('*' if (i,j) in loop else '.' for j in I2) for i in I1)
+        print disp
+        theta = min(x[i, j] for i, j in loop if x[i, j] > 0 and loop.index((i,j)) % 2 == 1)
+        for k in range(len(loop)):
+            if x[loop[k]] == 0:
+                Ub.append(loop[k])
+            print loop[k], ('+=' if k % 2 == 0 else '-='), theta
+            x[loop[k]] += theta * (1 if k % 2 == 0 else -1)
+            if x[loop[k]] == 0 and loop[k] in Ub:
+                Ub.remove(loop[k])
+
+
+print transport(A, B, C)
